8644e7dfa3d819eb0a4d4b71e9f0466af2353ef8
work2win <worknpeace2win@gmail.com>
2024-05-24 14:15:52 -0400

    model update

--- src/main/java/com/work2win/model/Transaction.java
+++ src/main/java/com/work2win/model/Transaction.java
@@ -8,15 +8,18 @@ import org.springframework.data.cassandra.core.mapping.Table;
 
 import lombok.Builder;
 import lombok.Data;
+import java.util.UUID;
 
 @Data
 @Builder
 @Table("transaction")
 public class Transaction {
 	
+	
+	
 	@PrimaryKey
-	private int id;
 	private int accountnum;
+	private UUID transactionID;
 	private Date date;
 	private String refOrcheckNo;
 	private float debit;
@@ -27,62 +30,75 @@ public class Transaction {
 	public Transaction() {}
 	
 	@Autowired
-	public Transaction(int id, int accountnum, Date date, String refOrcheckNo, float debit, float credit,
+	public Transaction(int accountnum, UUID transactionID, Date date, String refOrcheckNo, float debit, float credit,
 			float balance) {
 		super();
-		this.id = id;
 		this.accountnum = accountnum;
+		this.transactionID = transactionID;
 		this.date = date;
 		this.refOrcheckNo = refOrcheckNo;
 		this.debit = debit;
 		this.credit = credit;
 		this.balance = balance;
 	}
-	
-	public int getId() {
-		return id;
-	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-	
 	public int getAccountnum() {
 		return accountnum;
 	}
+
 	public void setAccountnum(int accountnum) {
 		this.accountnum = accountnum;
 	}
+
+	public UUID getTransactionID() {
+		return transactionID;
+	}
+
+	public void setTransactionID(UUID transactionID) {
+		this.transactionID = transactionID;
+	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	public String getRefOrcheckNo() {
 		return refOrcheckNo;
 	}
+
 	public void setRefOrcheckNo(String refOrcheckNo) {
 		this.refOrcheckNo = refOrcheckNo;
 	}
+
 	public float getDebit() {
 		return debit;
 	}
+
 	public void setDebit(float debit) {
 		this.debit = debit;
 	}
+
 	public float getCredit() {
 		return credit;
 	}
+
 	public void setCredit(float credit) {
 		this.credit = credit;
 	}
+
 	public float getBalance() {
 		return balance;
 	}
+
 	public void setBalance(float balance) {
 		this.balance = balance;
 	}
+	
+	
 	
 	
 
